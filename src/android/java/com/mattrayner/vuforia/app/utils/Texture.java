9e1a9b5ea1835db113078e948455eb0a1df93d5a
stevedj <djojokusumo.stevanus@gmail.com>
2017-04-18 19:31:07 +0700

    missing scan line

--- src/android/java/com/mattrayner/vuforia/app/utils/Texture.java
+++ src/android/java/com/mattrayner/vuforia/app/utils/Texture.java
@@ -1,11 +1,13 @@
 /*===============================================================================
+Copyright (c) 2016 PTC Inc. All Rights Reserved.
+
 Copyright (c) 2012-2014 Qualcomm Connected Experiences, Inc. All Rights Reserved.
 
-Vuforia is a trademark of QUALCOMM Incorporated, registered in the United States 
-and other countries. Trademarks of QUALCOMM Incorporated are used with permission.
+Vuforia is a trademark of PTC Inc., registered in the United States and other 
+countries.
 ===============================================================================*/
 
-package com.mattrayner.vuforia.app.utils;
+package com.vuforia.samples.SampleApplication.utils;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
